b36a4ce8f3097a0beb04a9692bb39f86a46af522
Jose Samaniego <Jose.Samaniego@howden.com>
2022-01-20 17:07:38 -0300

    Inicializar ListView con ofertas en edicion

--- ViewModels/Aplicacion/DMOfertasViewModel.cs
+++ ViewModels/Aplicacion/DMOfertasViewModel.cs
@@ -18,6 +18,8 @@ namespace HCHLView.ViewModels.Aplicacion
             ListaOfertas = new ObservableCollection<OfertaCompleta>();
 
             this._repOferta = repOferta;
+
+            _seleccion = "Editando";
         }
 
         private string _seleccion;
