4eccf7faee5d832da2d0dc089df0ed154dee37a5
Jose Samaniego <Jose.Samaniego@howden.com>
2022-01-20 15:41:34 -0300

    reestructuracion

--- Views/Aplicacion/DMOfertas.xaml.cs
+++ Views/Aplicacion/DMOfertas.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace HCHLView.Views
+namespace HCHLView.Views.Aplicacion
 {
     /// <summary>
     /// Lógica de interacción para DMOfertas.xaml
