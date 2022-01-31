133d904d3b9dd4bba03e9439fc81a7ba04e6087e
Jose Samaniego <Jose.Samaniego@howden.com>
2022-01-31 19:01:23 -0300

    Varios

--- ViewModels/MenuInicioViewModel.cs
+++ ViewModels/MenuInicioViewModel.cs
@@ -1,32 +1,193 @@
-﻿using HCHLView.Views.BBDD;
+﻿using Data;
+using Data.Repositorios;
+using HCHLView.Views.Aplicacion;
+using HCHLView.Views.BBDD;
+using HCHLView.Views.BBII;
+using Model.ReadModel;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace HCHLView.ViewModels
 {
-    public class MenuInicioViewModel : BindableBase
+    public class MenuInicioViewModel : ViewModelBase
     {
-        public Visibility Hide { get; set; }
-        public DelegateCommand Navigate { get; private set; }
+        private bool _isAppSelected { get; set; }
+        public bool IsAppSelected
+        {
+            get
+            {
+                return _isAppSelected;
+            }
+
+            set
+            {
+                _isAppSelected = value;
+                if (_isAppSelected)
+                {
+                    IsBIVisible = Visibility.Collapsed;
+                    IsBDVisible = Visibility.Collapsed;
+                    IsSpAplicacionVisible = Visibility.Visible;
+                    IsBorderVisible = Visibility.Visible;
+
+                    TbAppContent = "Ingeniería de Aplicación";
+                }
+                else
+                {
+                    IsBIVisible = Visibility.Visible;
+                    IsBDVisible = Visibility.Visible;
+                    IsSpAplicacionVisible = Visibility.Collapsed;
+                    IsBorderVisible = Visibility.Collapsed;
+                
+                    TbAppContent = "Aplicación";
+                }
+
+                RaisePropertyChanged(nameof(IsBIVisible));
+                RaisePropertyChanged(nameof(IsBDVisible));
+                RaisePropertyChanged(nameof(IsSpAplicacionVisible));
+                RaisePropertyChanged(nameof(IsBorderVisible));
+                RaisePropertyChanged(nameof(TbAppContent));
+            }
+        }
+
+        private bool _isBISelected { get; set; }
+        public bool IsBISelected
+        {
+            get
+            {
+                return _isBISelected;
+            }
+
+            set
+            {
+                _isBISelected = value;
+                if (_isBISelected)
+                {
+                    IsAppVisible = Visibility.Collapsed;
+                    IsBDVisible = Visibility.Collapsed;
+                }
+                else
+                {
+                    IsAppVisible = Visibility.Visible;
+                    IsBDVisible = Visibility.Visible;
+                }
+
+                RaisePropertyChanged(nameof(IsAppVisible));
+                RaisePropertyChanged(nameof(IsBDVisible));
+            }
+        }
+
+        private bool _isBDSelected { get; set; }
+        public bool IsBDSelected
+        {
+            get
+            {
+                return _isBDSelected;
+            }
+
+            set
+            {
+                _isBDSelected = value;
+                if (_isBDSelected)
+                {
+                    IsAppVisible = Visibility.Collapsed;
+                    IsBIVisible = Visibility.Collapsed;
+                    IsGvBDVisible = Visibility.Visible;
+                }
+                else
+                {
+                    IsAppVisible = Visibility.Visible;
+                    IsBIVisible = Visibility.Visible;
+                    IsGvBDVisible = Visibility.Collapsed;
+                }
+
+                RaisePropertyChanged(nameof(IsAppVisible));
+                RaisePropertyChanged(nameof(IsBIVisible));
+                RaisePropertyChanged(nameof(IsGvBDVisible));
+            }
+        }
+
+        private bool _isDMOfertasSelected { get; set; }
+        public bool IsDMOfertasSelected
+        {
+            get
+            {
+                return _isDMOfertasSelected;
+            }
+
+            set
+            {
+                _isDMOfertasSelected = value;
+                if (_isDMOfertasSelected)
+                {
+                    IsSpInicioVisible = Visibility.Collapsed;
+                    IsBorderVisible = Visibility.Collapsed;
+                }
+                else
+                {
+                    IsSpInicioVisible = Visibility.Visible;
+                    IsBorderVisible = Visibility.Visible;
+                }
+
+                RaisePropertyChanged(nameof(IsSpInicioVisible));
+                RaisePropertyChanged(nameof(IsBorderVisible));
+            }
+        }
+
+        public string Buscador { get; set; }
+        public Visibility IsSpInicioVisible { get; set; }
+        public Visibility IsSpAplicacionVisible { get; set; }
+        public Visibility IsGvBDVisible { get; set; }
+        public Visibility IsBorderVisible { get; set; }
+        public Visibility IsBIVisible { get; set; }
+        public Visibility IsAppVisible { get; set; }
+        public Visibility IsBDVisible { get; set; }
+        public string TbAppContent { get; set; }
+        public DelegateCommand NavigateAccesoBD { get; private set; }
+        public DelegateCommand NavigateAccesoBI { get; private set; }
+        public DelegateCommand NavigateDMOfertas { get; private set; }
 
         private readonly IRegionManager regionManager;
 
-        public MenuInicioViewModel(IRegionManager regionManager)
+        public MenuInicioViewModel(IRegionManager regionManager, RepositorioTablasBD repositorioTablasBD)
         {
-            Hide = Visibility.Visible;
+            IsBIVisible = Visibility.Visible;
+            IsAppVisible = Visibility.Visible;
+            IsBDVisible = Visibility.Visible;
+            IsSpAplicacionVisible = Visibility.Collapsed;
+            IsGvBDVisible = Visibility.Collapsed;
+            IsBorderVisible = Visibility.Collapsed;
+            TbAppContent = "Aplicación";
+
             this.regionManager = regionManager;
 
-            Navigate = new DelegateCommand(NavigateMethod);
+            NavigateAccesoBD = new DelegateCommand(NavigateAccesoBDMethod);
+            NavigateAccesoBI = new DelegateCommand(NavigateAccesoBIMethod);
+            NavigateDMOfertas = new DelegateCommand(NavigateDMOfertasMethod);
         }
 
-        private void NavigateMethod()
+        private void NavigateDMOfertasMethod()
         {
-            regionManager.RequestNavigate("ContentRegion", nameof(AccesoBD));
+            regionManager.RequestNavigate("ContentRegion", nameof(DMOfertasView));
 
-            Hide = Visibility.Collapsed;
-            RaisePropertyChanged(nameof(Hide));
+            RaisePropertyChanged(nameof(IsDMOfertasSelected));
         }
+
+        private void NavigateAccesoBDMethod()
+        {
+            regionManager.RequestNavigate("ContentRegion", nameof(AccesoBDView));
+
+            RaisePropertyChanged(nameof(IsBDSelected));
+        }
+
+        private void NavigateAccesoBIMethod()
+        {
+            regionManager.RequestNavigate("ContentRegion", nameof(AccesoBIView));
+
+            RaisePropertyChanged(nameof(IsBISelected));
+        }
+
     }
 }
