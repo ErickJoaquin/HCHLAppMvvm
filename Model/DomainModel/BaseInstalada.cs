0052d1b6620629d468f234299ea09c2e707b1866
Jose Samaniego <Jose.Samaniego@howden.com>
2022-02-02 19:55:50 -0300

    Override ToString en BaseInstalada

--- Model/DomainModel/BaseInstalada.cs
+++ Model/DomainModel/BaseInstalada.cs
@@ -25,11 +25,6 @@ namespace Model
         public string InformacionExtra { get; set; }
         [Computed]
         public bool seleccion { get; set; } // Para ver si esta incluido o no en la oferta
-
-        public override string ToString()
-        {
-            return $"{Modelo} - {NSerie}";
-        }
     }
 }
 
