535a7aeda7ca41682d2246455126baae92805ad1
Jose Samaniego <Jose.Samaniego@howden.com>
2022-02-13 19:04:46 -0300

    TipoIDTributario

--- Model/ReadModel/AccesoBIModel.cs
+++ Model/ReadModel/AccesoBIModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model.Utilidades;
 
 namespace Model.ReadModel
 {
@@ -14,6 +15,7 @@ namespace Model.ReadModel
         public string Modelo { get; set; }
         public string NSerie { get; set; }
         public string RefCiente { get; set; }
+        public string Pais { get; set; }
         public string Nombre { get; set; }
         public string Planta { get; set; }
         [Computed]
@@ -30,19 +32,28 @@ namespace Model.ReadModel
                     return "Unknown";
                 }
             }
-
-            set
+        }
+        [Computed]
+        public string TipoIDTributario
+        {
+            get
             {
-
+                if (EndUser.Pais != null)
+                {
+                    return IDTributario.GetType(EndUser.Pais);
+                }
+                else
+                {
+                    return "Unknown";
+                }
             }
         }
-        public string Pais { get; set; }
-
-
         public BaseInstalada Equipo { get; set; }
         public EndUser EndUser { get; set; }
         public BU Proveedor { get; set; }
         public Proceso Proceso { get; set; }
+        public Mercado Mercado { get; set; }
+        public EquiposCRM CRM { get; set; }
         public List<Oferta> Ofertas { get; set; }
         public List<Venta> Ventas { get; set; }
         public List<Servicio> Servicios { get; set; }
---
+++ Servicio/Utilidades/IDTributario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio.Utilidades
+{
+    public class IDTributario
+    {
+        public string GetType(string country)
+        {
+            string Type = "";
+            if (country == "AR") { Type = "CUIT:"; }
+            else if (country == "BR") { Type = "CPF/CNPJ:"; }
+            else if (country == "VE") { Type = "RIF:"; }
+            else if (country == "CL" || country == "UY") { Type = "RUT:"; }
+            else if (country == "CR") { Type = "NITE:"; }
+            else if (country == "DO") { Type = "RNC:"; }
+            else if (country == "BO" || country == "CO" || country == "PAN" || country == "ES" || country == "GU") { Type = "NIT:"; }
+            else if (country == "PA" || country == "PE" || country == "NI" || country == "EC") { country = "RUC:"; }
+            else if (country == "HN") { Type = "RTN:"; }
+
+            return Type;
+        }
+    }
+}
