90ea96f05709e6ea1cb3034eb5043e69ea0c18df
Jose Samaniego <Jose.Samaniego@howden.com>
2022-02-14 18:17:58 -0300

    Agregar necesarios

---
+++ HCHLViewModule.cs
@@ -0,0 +1,23 @@
+﻿using HCHLView.Views.Aplicacion;
+using HCHLView.Views.BBII;
+using HCHLView.Views.BBDD;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace HCHLView
+{
+    public class HCHLViewModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<AccesoBIView>();
+            containerRegistry.RegisterForNavigation<AccesoBDView>();
+            containerRegistry.RegisterForNavigation<DMOfertasView>();
+        }
+    }
+}
